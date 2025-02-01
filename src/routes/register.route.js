c8d9ebe0ac1ad3140194eb139b8a41123a972637
Ayush Kumar Singh <shkmrsingh@gmail.com>
2025-02-01 18:04:50 +0530

    get changes

--- src/routes/register.route.js
+++ src/routes/register.route.js
@@ -4,6 +4,9 @@ const createUser = require("../controllers/register.controller")
 const registerRoute = express.Router()
 
 registerRoute.post("/",createUser)
+registerRoute.get("/",(req,res)=>{
+    res.send({message : "hi i'm working fine now."})
+})
 
 
 module.exports = registerRoute
